1308654c1f1d6034197d7dc5410e965d820ec58a
Jarid Margolin <jaridmargolin@gmail.com>
2014-04-04 16:02:58 -0700

    Bump to 5.1.2

--- build/iscroll-zoom.js
+++ build/iscroll-zoom.js
@@ -1,4 +1,4 @@
-/*! iScroll v5.1.1 ~ (c) 2008-2014 Matteo Spinelli ~ http://cubiq.org/license */
+/*! iScroll v5.1.2 ~ (c) 2008-2014 Matteo Spinelli ~ http://cubiq.org/license */
 (function (window, document, Math) {
 var rAF = window.requestAnimationFrame	||
 	window.webkitRequestAnimationFrame	||
@@ -327,7 +327,7 @@ function IScroll (el, options) {
 }
 
 IScroll.prototype = {
-	version: '5.1.1',
+	version: '5.1.2',
 
 	_init: function () {
 		this._initEvents();
